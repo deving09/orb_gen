79a4c160b37f9a7a373e1fd1f250231b6e0ebbb9
Devin Guillory <deving2009@gmail.com>
2022-07-19 18:25:27 +0000

    add both test

---
+++ src/test.py
@@ -0,0 +1,14 @@
+import torch
+import torchvision
+import tqdm
+
+
+
+print("Starting Test")
+
+for i in range(1000000):
+    if  i%1000 == 0:
+        print("step :%d completed" %i)
+
+print("Test Ended")
+
