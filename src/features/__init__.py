dd8c5d39f6da9f0ab6e08140bca35a71c95e0c1b
Devin G <deving2009@gmail.com>
2022-07-25 10:44:39 -0700

    updates for various clip models

--- src/features/__init__.py
+++ src/features/__init__.py
@@ -3,11 +3,14 @@
 
 from features.resnet import resnet18, resnet18_84
 from features.efficientnet import efficientnetb0
-from features.clipimf import clip_vitb_32
+from features.clipimf import clip_vitb_32, clip_vitb_16, clip_resnet50, clip_resnet101
 
 extractors = {
         'resnet18': resnet18,
         'resnet18_84': resnet18_84,
         'efficientnetb0' : efficientnetb0,
-        'clip_vitb_32': clip_vitb_32
+        'clip_vitb_32': clip_vitb_32,
+        'clip_vitb_16': clip_vitb_16,
+        'clip_resnet50': clip_resnet50,
+        'clip_resnet101': clip_resnet101
         }
